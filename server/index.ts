bcc6293c3d8857f8970d6228bf9b75f14f1c49c7
Asadullah325 <asadullah456545@gmail.com>
2025-04-19 20:56:00 +0500

    new changes

--- server/index.ts
+++ server/index.ts
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL,
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
